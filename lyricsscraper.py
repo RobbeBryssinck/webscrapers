49258794793ca5d5925332e884c7c12308e09250
Robbe Bryssinck <robbebryssinck@gmail.com>
2020-01-07 11:32:13 +0100

    Refactoring and error handling

--- lyricsscraper.py
+++ lyricsscraper.py
@@ -4,9 +4,14 @@ import requests
 
 
 def get_lyrics(song, artists):
-	num_page = 1
-	search_results = google.search(f"site:azlyrics.com {song} {artists}", num_page)
-	lyricslink = search_results[0].link
+	try:
+		num_page = 1
+		search_results = google.search(f"site:azlyrics.com {song} {artists}", num_page)
+		lyricslink = search_results[0].link
+	except Exception as e:
+		print("Something went wrong gathering the lyrics.")
+		print("Check if you have a working internet connection.")
+		return
 
 	source = requests.get(lyricslink).text
 	soup = BeautifulSoup(source, 'lxml')
@@ -14,24 +19,37 @@ def get_lyrics(song, artists):
 	lyricsdiv = soup.find('div', class_="col-xs-12 col-lg-8 text-center")
 	lyrics = lyricsdiv.find('div', class_=None).text
 
+	lyrics_available = does_song_have_lyrics(lyrics)
+
+	if lyrics_available:
+		return "Lyrics:\n" + lyrics
+	else:
+		return "This song doesn't have any lyrics!"
+
+
+def does_song_have_lyrics(lyrics):
+	'''
+	AZLyrics.com still has pages up for songs without lyrics.
+	These pages list the related album for other songs.
+	Therefore, if the scraper finds that the scraped content contains the word
+	'album' at in the first 6 characters, the program returns "no lyrics".
+	'''
+
 	i = 0
 	lyricsbuffer = ""
 	while i < 6:
 		lyricsbuffer += lyrics[i]
 		i += 1
 	
-	# AZLyrics.com still has pages up for songs without lyrics.
-	# These pages list the related album for other songs.
-	# Therefore, if the scraper finds that the scraped content contains the word
-	# 'album' at in the first 6 characters, the program returns "no lyrics".
+	
 	if 'album' in lyricsbuffer.lower():
-		return "This song doesn't have any lyrics!"
-
-	return "Lyrics:\n" + lyrics
+		return False
+	else:
+		return True
 
 
 def main():
-	print("Lyrics scrapper 1.0")
+	print("Lyrics scrapper 1.1")
 
 	print("Song name:")
 	song = input('> ')
